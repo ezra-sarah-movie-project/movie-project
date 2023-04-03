19b44256c523a908ad82a927069a77c29e7908e3
Ezra Marama <ezra.marama@gmail.com>
2023-04-03 15:49:05 -0500

    updating renderMovie function

--- js/movies.js
+++ js/movies.js
@@ -1,3 +1,13 @@
+
+window.addEventListener('load',()=>{
+    const loader = document.querySelector('.loading');
+    loader.classList.add('loading-hidden');
+    loader.addEventListener('transitioned', ()=>{
+        document.body.removeChild("loading");
+    });
+})
+
+
 export const getFavorites = async () => {
     try {
         let url = `http://localhost:3000/favorites`;
@@ -110,5 +120,46 @@ export const deleteFavorite = async (id) => {
     } catch(error){
         console.log(error);
     }
+}
+
+export const renderMovieCards = (movie, parent) => {
+    movie.forEach(movie => {
+        const element = document.createElement('div');
+        if (movie.id === 1) {
+            element.classList.add('slide','active');
+        } else {
+            element.classList.add('slide','right');
+        }
+        element.innerHTML = `
+            <div  class="column shrink movie-container">
+                <div class="column shrink movie-container-left">
+                  <div class="column shrink align-center">
+                    <img src=${movie.image} alt="" class="movie-poster">
+                  </div>
+                </div>
+                <div class="column shrink movie-container-right">
+                  <h2>${movie.title}</h2>
+                  <p>---star rating---</p>
+                  <p>${movie.description}</p>
+                  <h3>Cast:</h3>
+                  <div class="cast-wrapper">
+                    <img src="https://image.tmdb.org/t/p/original/lyUyVARQKhGxaxy0FbPJCQRpiaW.jpg" alt="Ryan Gosling" class="cast-avatar">
+                    <img src="https://image.tmdb.org/t/p/original/lyUyVARQKhGxaxy0FbPJCQRpiaW.jpg" alt="Ryan Gosling" class="cast-avatar">
+                    <img src="https://image.tmdb.org/t/p/original/lyUyVARQKhGxaxy0FbPJCQRpiaW.jpg" alt="Ryan Gosling" class="cast-avatar">
+                    <img src="https://image.tmdb.org/t/p/original/lyUyVARQKhGxaxy0FbPJCQRpiaW.jpg" alt="Ryan Gosling" class="cast-avatar">
+                    <img src="https://image.tmdb.org/t/p/original/lyUyVARQKhGxaxy0FbPJCQRpiaW.jpg" alt="Ryan Gosling" class="cast-avatar">
+                  </div>
+                  <div class="movie-container-buttons">
+                    <button type="button">Watch Trailer</button>
+                    <button type="button">Play Now</button>
+                  </div>
+                </div>
+            </div>
+        `;
+        element.querySelector('button').addEventListener('click', function () {
+            element.remove();
+        });
+        parent.appendChild(element);
+    })
 }
 
