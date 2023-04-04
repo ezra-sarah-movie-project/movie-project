47f762a327156315757cd2cd0c05e2a892b370ec
zion segovia <zionsegovia@gmail.com>
2023-04-04 13:50:11 -0500

    Movie Cards Added

--- js/moviesApi.js
+++ js/moviesApi.js
@@ -1,13 +1,47 @@
-import {keys} from './keys.js'
+let MOVIE_API = '06c433449ce8de51aea9744d39596d5f'
 
-export const getMovies = async()=>{
-    try{
-        let response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${keys.MOVIES_APPID}`)
+export const movieListContainer = document.getElementById("movie-compact-list");
+
+export const getMovies = async () => {
+    try {
+        let response = await fetch(`https://api.themoviedb.org/3/discover/movie?api_key=${MOVIE_API}`);
         let data = await response.json();
-        // console.log(getMovies.data[0].title);
-        return data;
-
-    }catch(error){
+        const movies = data.results.slice(0, 10).map((movie) => {
+            return {
+                title: movie.title,
+                rating: movie.vote_average,
+                description: movie.overview,
+                banner: `https://image.tmdb.org/t/p/original/${movie.poster_path}`,
+            };
+        });
+        return movies;
+    } catch (error) {
         console.log(error);
     }
-}
+};
+
+const displayMovies = async () => {
+    const movies = await getMovies();
+    movies.forEach((movie, index) => {
+        const movieContainer = document.createElement("div");
+        movieContainer.classList.add("movieStuff");
+        movieContainer.id = `movieData${index + 1}`;
+        const banner = document.createElement("img");
+        banner.src = movie.banner;
+        banner.classList.add("movieBanner");
+        const title = document.createElement("div");
+        title.classList.add("title");
+        title.textContent = movie.title;
+        const rating = document.createElement("div");
+        rating.classList.add("Review");
+        rating.textContent = `Rating: ${movie.rating}`;
+
+        movieContainer.appendChild(banner);
+        movieContainer.appendChild(title);
+        movieContainer.appendChild(rating);
+
+        movieListContainer.appendChild(movieContainer);
+    });
+};
+
+displayMovies();
