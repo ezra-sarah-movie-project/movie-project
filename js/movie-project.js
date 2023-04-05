bbe4ef5ecfca0d47e766d7b20282abda2ae17bdd
Ezra Marama <ezra.marama@gmail.com>
2023-04-04 19:03:18 -0500

    refactored js code and commented on functionality

--- js/movie-project.js
+++ js/movie-project.js
@@ -1,4 +1,4 @@
-import {setFavorite, patchFavorite, deleteFavorite, getFavorites, getFavorite, renderMovieCards} from "./movies.js";
+import {setFavorite, patchFavorite, deleteFavorite, getFavorites, getFavorite, renderMovieCards, newUserMovie, userDeleteSubmit, userPatchSubmit} from "./movies.js";
 import {getMovies} from "./moviesApi.js";
 
 
@@ -11,114 +11,19 @@ import {getMovies} from "./moviesApi.js";
     let parent = document.querySelector('.slides');
     await renderMovieCards(favorites, parent);
 
-    let userMovieTitle = document.querySelector('#movie-title-input');
-    let userMovieRating = document.querySelector('#rating-input');
-    let userMovieGenre = document.querySelector('#genre-input');
-    let userMovieDescription = document.querySelector('#description-input');
+    //target user movie submit button
     let userMovieSubmit = document.querySelector('#movie-submit-button');
+    // store user entered movie into database using post method
+    userMovieSubmit.addEventListener('click', newUserMovie);
 
-    userMovieSubmit.addEventListener('click', async (event)=>{
-        event.preventDefault();
-        let userMovie = {
-            title: userMovieTitle.value,
-            genre: userMovieGenre.value,
-            rating: userMovieRating.value,
-            description: userMovieDescription.value,
-        }
-        await setFavorite(userMovie);
-    });
-
-    // Code to get the delete movie button to delete a movie
+    // Code to get the delete movie button to delete a movie by id
     let deleteButton = document.querySelector('#delete-movie-btn');
-    deleteButton.addEventListener('click', async (event) => {
-        event.preventDefault();
-        let id = document.querySelector('#delete-by-id').value;
-        let deletedMovie = await deleteFavorite(id);
-        console.log(`Movie with ID ${id} deleted:`, deletedMovie);
-    });
+    // delete movie by id that user entered
+    deleteButton.addEventListener('click',userDeleteSubmit);
 
-    let userPatchId = document.querySelector('#patch-id');
-    let userPatchTitle = document.querySelector('#patch-title');
-    let userPatchRating = document.querySelector('#patch-rating');
-    let userPatchGenre = document.querySelector('#patch-genre');
-    let userPatchDescription = document.querySelector('#patch-description');
+    // target the patch button to submit changes
     let userPatchButton = document.querySelector('#patch-button');
-
-    userPatchButton.addEventListener('click', async (event)=>{
-        event.preventDefault();
-        let movieToUpdate = await getFavorite(userPatchId.value);
-
-        let update = {
-            title: userPatchTitle.value ? userPatchTitle.value : movieToUpdate.title,
-            genre: userPatchGenre.value ? userPatchGenre.value : movieToUpdate.genre,
-            rating: userPatchRating.value ? userPatchRating.value: movieToUpdate.rating,
-            description: userPatchDescription.value ? userPatchDescription.value: movieToUpdate.description
-        }
-        await patchFavorite(userPatchId.value,update);
-    });
-
-
-    //CAROUSEL FUNCTIONALITY
-    let arrows = document.querySelectorAll('.arrow');
-
-    arrows.forEach(function(arrow){
-        // start a direction variable
-        let direction;
-        let slideMask = document.querySelector('.slider-mask');
-        let slidesContainer = document.querySelector('.slides');
-        arrow.addEventListener('click', function(event){
-            // if the arrow clicked has a class of left, set direction to left, otherwise set to right
-            if (event.target.classList.contains('arrow-left')) {
-                direction = 'left';
-            } else {
-                direction = 'right';
-            }
-            // get the active slide
-            let activeSlide = slideMask.querySelector('.slide.active');
-            // remove active class from active slide
-            activeSlide.classList.remove('active');
-            // if direction is left, get previous sibling, otherwise get next sibling and reassign to activeSlide
-            if (direction === 'left') {
-                activeSlide = activeSlide.previousElementSibling;
-            } else {
-                activeSlide = activeSlide.nextElementSibling;
-            }
-            //if activeSlide is null, we are at the end of the carousel
-            if (!activeSlide) {
-                //if direction is left, get last slide, otherwise get first slide
-                activeSlide = direction === 'left' ? slidesContainer.lastElementChild : slidesContainer.firstElementChild;
-            }
-            //add active to the new active slide
-            activeSlide.classList.add('active');
-            // remove left and right classes from active slide
-            activeSlide.classList.remove('left', 'right');
-
-            let previousSibling = activeSlide.previousElementSibling;
-            //get all previous element siblings
-            let previousSlides = [];
-            while (previousSibling) {
-                previousSlides.unshift(previousSibling);
-                previousSibling = previousSibling.previousElementSibling;
-            }
-            //loop through previous slides and add left class, remove right class
-            previousSlides.forEach(function(slide){
-                slide.classList.remove('right');
-                slide.classList.add('left');
-            });
-            // get next sibling of active slide
-            let nextSibling = activeSlide.nextElementSibling;
-            // get all next element siblings
-            let nextSlides = [];
-            while (nextSibling) {
-                nextSlides.push(nextSibling);
-                nextSibling = nextSibling.nextElementSibling;
-            }
-            // loop through next slides and add right class, remove left class
-            nextSlides.forEach(function(slide){
-                slide.classList.remove('left');
-                slide.classList.add('right');
-            });
-        });
-    });
+    // update the selected movie based on user inputs
+    userPatchButton.addEventListener('click',userPatchSubmit);
 
 })();
--- js/movies.js
+++ js/movies.js
@@ -100,7 +100,7 @@ export const patchFavorite = async (id, movie) => {
         console.log(error);
     }
 }
-
+// delete database object by id
 export const deleteFavorite = async (id) => {
     try {
         if (!id) {
@@ -121,6 +121,7 @@ export const deleteFavorite = async (id) => {
     }
 }
 
+// render favorite movies in carousel
 export const renderMovieCards = (movie, parent) => {
     movie.forEach(movie => {
         const element = document.createElement('div');
@@ -141,8 +142,6 @@ export const renderMovieCards = (movie, parent) => {
                   <p>---star rating---</p>
                   <p>${movie.description}</p>
                   <div class="movie-container-buttons">
-                    
-                    
                   </div>
                   <button type="button">Watch Trailer</button>
                 </div>
@@ -155,3 +154,141 @@ export const renderMovieCards = (movie, parent) => {
     })
 }
 
+//create new favorite movie and save to database using "POST" method
+ export const newUserMovie = async (event)=> {
+    event.preventDefault();
+     let userMovieTitle = document.querySelector('#movie-title-input');
+     let userMovieRating = document.querySelector('#rating-input');
+     let userMovieGenre = document.querySelector('#genre-input');
+     let userMovieDescription = document.querySelector('#description-input');
+
+    let userMovie = {
+        title: userMovieTitle.value,
+        genre: userMovieGenre.value,
+        rating: userMovieRating.value,
+        description: userMovieDescription.value,
+    }
+    await setFavorite(userMovie);
+}
+//delete favorite movie from database using "DELETE" method
+export const userDeleteSubmit = async (event) => {
+    event.preventDefault();
+    let id = document.querySelector('#delete-by-id').value;
+    let deletedMovie = await deleteFavorite(id);
+    console.log(`Movie with ID ${id} deleted:`, deletedMovie);
+}
+
+// user edited movie submission sent using 'PATCH' method
+export const userPatchSubmit = async (event) => {
+    //prevent default submission
+    event.preventDefault();
+    // target ID patch input
+    let userPatchId = document.querySelector('#patch-id');
+    // target patch title input
+    let userPatchTitle = document.querySelector('#patch-title');
+    // target user rating input
+    let userPatchRating = document.querySelector('#patch-rating');
+    // target user genre input
+    let userPatchGenre = document.querySelector('#patch-genre');
+    // target user description input
+    let userPatchDescription = document.querySelector('#patch-description');
+    // get the favorite movie object by ID
+    let movieToUpdate = await getFavorite(userPatchId.value);
+    //update the favorite movie depending on the user entered fields, use current info if empty
+    let update = {
+        title: userPatchTitle.value ? userPatchTitle.value : movieToUpdate.title,
+        genre: userPatchGenre.value ? userPatchGenre.value : movieToUpdate.genre,
+        rating: userPatchRating.value ? userPatchRating.value: movieToUpdate.rating,
+        description: userPatchDescription.value ? userPatchDescription.value: movieToUpdate.description
+    }
+    //patch request sent to database
+    await patchFavorite(userPatchId.value,update);
+}
+
+//CAROUSEL FUNCTIONALITY
+let arrows = document.querySelectorAll('.arrow');
+// carousel forEach
+arrows.forEach(function(arrow){
+    // start a direction variable
+    let direction;
+    let slideMask = document.querySelector('.slider-mask');
+    let slidesContainer = document.querySelector('.slides');
+    arrow.addEventListener('click', function(event){
+        // if the arrow clicked has a class of left, set direction to left, otherwise set to right
+        if (event.target.classList.contains('arrow-left')) {
+            direction = 'left';
+        } else {
+            direction = 'right';
+        }
+        // get the active slide
+        let activeSlide = slideMask.querySelector('.slide.active');
+        // remove active class from active slide
+        activeSlide.classList.remove('active');
+        // if direction is left, get previous sibling, otherwise get next sibling and reassign to activeSlide
+        if (direction === 'left') {
+            activeSlide = activeSlide.previousElementSibling;
+        } else {
+            activeSlide = activeSlide.nextElementSibling;
+        }
+        //if activeSlide is null, we are at the end of the carousel
+        if (!activeSlide) {
+            //if direction is left, get last slide, otherwise get first slide
+            activeSlide = direction === 'left' ? slidesContainer.lastElementChild : slidesContainer.firstElementChild;
+        }
+        //add active to the new active slide
+        activeSlide.classList.add('active');
+        // remove left and right classes from active slide
+        activeSlide.classList.remove('left', 'right');
+
+        let previousSibling = activeSlide.previousElementSibling;
+        //get all previous element siblings
+        let previousSlides = [];
+        while (previousSibling) {
+            previousSlides.unshift(previousSibling);
+            previousSibling = previousSibling.previousElementSibling;
+        }
+        //loop through previous slides and add left class, remove right class
+        previousSlides.forEach(function(slide){
+            slide.classList.remove('right');
+            slide.classList.add('left');
+        });
+        // get next sibling of active slide
+        let nextSibling = activeSlide.nextElementSibling;
+        // get all next element siblings
+        let nextSlides = [];
+        while (nextSibling) {
+            nextSlides.push(nextSibling);
+            nextSibling = nextSibling.nextElementSibling;
+        }
+        // loop through next slides and add right class, remove left class
+        nextSlides.forEach(function(slide){
+            slide.classList.remove('left');
+            slide.classList.add('right');
+        });
+    });
+
+
+    //MODAL POP UP FUNCTIONALITY
+    // target the modal
+    let modal = document.getElementById("myModal");
+    // target modal button
+    let btn = document.getElementById("myBtn");
+    // target span that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+    // display modal on click
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+    // when X is clicked, close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+    // click anywhere outside of modal close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+    //MODAL FUNCTIONALITY END
+
+});
